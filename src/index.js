3caec57a7267cfd0e38bc1213366128581a4f957
YuyaNakamura0139 <mtk371@icloud.com>
2021-11-05 19:13:43 +0900

    #4 improve_game_display_page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import GameDisplayPage from './GameDisplayPage';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GameDisplayPage />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
