bf87f098e3cb83fb7b24c5a6c9275f710b3125ff
奥田啓皓 <>
2021-11-06 05:04:24 +0900

    トップページを作成 スタイリングは保留 #11

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import Button from '@material-ui/core/Button';
+const Header = () => {
+  return(
+    <header>
+      <Button variant="contained" color="secondary">Settings</Button>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,23 @@
+import Button from '@material-ui/core/Button';
+import Header from '../components/Header';
+import './Home.css';
 const Home = () => {
   return (
-    <div>This is the Home page</div>
+    <div class="vh-100">
+      <Header />
+      <h1>Connect 4!</h1>
+      <div>
+        <div>
+          <input type="radio" name="difficulty"/>
+          <label>vs Player</label>
+        </div>
+        <div>
+          <input type="radio" name="difficulty"/>
+          <label>vs CPU</label>
+        </div>
+      </div>
+      <Button variant="contained" color="secondary">Start!</Button>
+    </div>
   );
 };
 export default Home;
