a47dc4da68e2130db59f559fab7a4ec7014d08dd
YuyaNakamura0139 <mtk371@icloud.com>
2021-11-07 15:30:01 +0900

    #15 ゲーム画面のクリックイベントを作成(途中)

--- src/pages/GameDisplayPage.js
+++ src/pages/GameDisplayPage.js
@@ -4,9 +4,14 @@ import './GameDisplayPage.css'
 
 const Cell = (props) => {
     let color = "white";
+    if (props.value === false) {
+        color = "red";
+    } else if (props.value === true) {
+        color = "yellow";
+    }
     return (
         <td>
-            <div className="cell" key={props.key}>
+            <div className="cell" data-columnindex={props.columnindex} onClick={props.onClick}>
                 <div className={color}></div>
             </div>
         </td>
@@ -15,8 +20,14 @@ const Cell = (props) => {
 
 const Row = (props) => {
     return (
-        <tr key={props.key}>
-            {props.row.map((cell, cellIndex) => (<Cell cell={cell} key={cellIndex}/>))}
+        <tr>
+            {props.row.map((cell, columnindex) => (
+                <Cell 
+                    value={cell} 
+                    key={columnindex} 
+                    columnindex={columnindex}  
+                    onClick={props.onClick} 
+                />))}
         </tr>
     );
 }
@@ -24,7 +35,14 @@ const Row = (props) => {
 const Board = (props) => {
     return (
         <table>
-            {props.board.map((row, rowIndex) => (<Row row={row} key={rowIndex}/>))}
+            <tbody>
+                {props.board.map((row, rowindex) => (
+                    <Row 
+                        row={row} 
+                        key={rowindex}
+                        onClick={props.onClick} 
+                    />))}
+            </tbody>
         </table>
     ); 
 }
@@ -37,10 +55,26 @@ const Button = (props) => {
 
 const GameDisplayPage = () => {
     const [board, setBoard] = useState(new Array(6).fill(Array(7).fill(null)));
+    const [nextPlayerIsRed, setNextPlayerIsRed] = useState(false);
 
     const initGame = () => {
         let board = new Array(6).fill(Array(7).fill(null))
         setBoard(prevState => board)
+        setNextPlayerIsRed(prevState => false)
+    }
+
+    const handleClick = (event) => {
+        let nextBoard = board.slice();
+        const dataset = event.currentTarget.dataset;
+        const columnIndex = dataset.columnindex;
+        for (let r=board.length-1; r >= 0; r--) {
+            if (nextBoard[r][columnIndex] == null) {
+                nextBoard[r][columnIndex] = nextPlayerIsRed;
+                setNextPlayerIsRed(prevState => !prevState)
+                break;
+            }
+        }
+        setBoard(prevState => nextBoard) 
     }
 
     return (
@@ -51,6 +85,7 @@ const GameDisplayPage = () => {
             />
             <Board 
                 board={board} 
+                onClick={handleClick}
             />
         </div>
     );
