c585a9365d0fa88ea2e5f6ec20d6e7bdc70c0b78
YuyaNakamura0139 <mtk371@icloud.com>
2021-11-11 19:49:15 +0900

    #15 クリックイベントを追加（途中）

--- src/pages/GameDisplayPage.js
+++ src/pages/GameDisplayPage.js
@@ -3,90 +3,78 @@ import { useState } from "react";
 import "./GameDisplayPage.css";
 
 const Cell = (props) => {
-    let color = "white";
-    if (props.value === false) {
-        color = "red";
-    } else if (props.value === true) {
-        color = "yellow";
-    }
-    return (
-        <td>
-            <div className="cell" data-columnindex={props.columnindex} onClick={props.onClick}>
-                <div className={color}></div>
-            </div>
-        </td>
-    );
-}
+  let color = "white";
+  if (props.value === false) {
+    color = "red";
+  } else if (props.value === true) {
+    color = "yellow";
+  }
+  return (
+    <td>
+      <div className="cell" data-columnindex={props.columnindex} onClick={props.onClick}>
+        <div className={color}></div>
+      </div>
+    </td>
+  );
+};
 
 const Row = (props) => {
-    return (
-        <tr>
-            {props.row.map((cell, columnindex) => (
-                <Cell 
-                    value={cell} 
-                    key={columnindex} 
-                    columnindex={columnindex}  
-                    onClick={props.onClick} 
-                />))}
-        </tr>
-    );
-}
+  return (
+    <tr>
+      {props.row.map((cell, columnindex) => (
+        <Cell value={cell} key={columnindex} columnindex={columnindex} onClick={props.onClick} />
+      ))}
+    </tr>
+  );
+};
 
 const Board = (props) => {
-    return (
-        <table>
-            <tbody>
-                {props.board.map((row, rowindex) => (
-                    <Row 
-                        row={row} 
-                        key={rowindex}
-                        onClick={props.onClick} 
-                    />))}
-            </tbody>
-        </table>
-    ); 
-}
+  return (
+    <table>
+      <tbody>
+        {props.board.map((row, rowindex) => (
+          <Row row={row} key={rowindex} onClick={props.onClick} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 const Button = (props) => {
   return <button onClick={props.onClick}>New Game</button>;
 };
 
 const GameDisplayPage = () => {
-    const [board, setBoard] = useState(new Array(6).fill(Array(7).fill(null)));
-    const [nextPlayerIsRed, setNextPlayerIsRed] = useState(false);
+  const [board, setBoard] = useState(new Array(6).fill(Array(7).fill(null)));
+  const [nextPlayerIsRed, setNextPlayerIsRed] = useState(false);
 
-    const initGame = () => {
-        let board = new Array(6).fill(Array(7).fill(null))
-        setBoard(board)
-        setNextPlayerIsRed(false)
+  const initGame = () => {
+    let board = new Array(6).fill(Array(7).fill(null));
+    setBoard(board);
+    setNextPlayerIsRed(false);
+  };
+
+  const handleClick = (event) => {
+    let nextBoard = board.slice();
+    const dataset = event.currentTarget.dataset;
+    const columnIndex = dataset.columnindex;
+    for (let row = board.length - 1; row >= 0; row--) {
+      if (nextBoard[row][columnIndex] == null) {
+        nextBoard[row][columnIndex] = nextPlayerIsRed;
+        setNextPlayerIsRed((prevState) => !prevState);
+        break;
+      }
     }
+    setBoard(nextBoard);
+  };
 
-    const handleClick = (event) => {
-        let nextBoard = board.slice();
-        const dataset = event.currentTarget.dataset;
-        const columnIndex = dataset.columnindex;
-        for (let r=board.length-1; r >= 0; r--) {
-            if (nextBoard[r][columnIndex] == null) {
-                nextBoard[r][columnIndex] = nextPlayerIsRed;
-                setNextPlayerIsRed(prevState => !prevState)
-                break;
-            }
-        }
-        setBoard(nextBoard) 
-    }
-
-    return (
-        <div className='game-display'>
-            <h1>Connect 4!</h1>
-            <Button 
-                onClick={initGame}
-            />
-            <Board 
-                board={board} 
-                onClick={handleClick}
-            />
-        </div>
-    );
-}
+  return (
+    <div className="game-display">
+      <h1>Connect 4!</h1>
+      <Button onClick={initGame} />
+      <Board board={board} onClick={handleClick} />
+    </div>
+  );
+};
 
 export default GameDisplayPage;
