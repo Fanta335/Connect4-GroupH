a598f67b74b1ae5dd52a89a3483e23c7aeaa2bc8
YuyaNakamura0139 <mtk371@icloud.com>
2021-11-12 18:29:59 +0900

    #15 勝利条件を追加（途中）

--- src/pages/GameDisplayPage.js
+++ src/pages/GameDisplayPage.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useState } from "react";
 import "./GameDisplayPage.css";
+import "./../utils/calculateWinner";
 import Modal from "../components/Modal";
+import calculateWinner from "./../utils/calculateWinner";
 
 const Cell = (props) => {
   let color = "white";
-  if (props.value === false) {
+  if (props.value === "Player1") {
     color = "red";
-  } else if (props.value === true) {
+  } else if (props.value === "Player2") {
     color = "yellow";
   }
   return (
@@ -57,14 +59,18 @@ const GameDisplayPage = () => {
   }
 
   const [board, setBoard] = useState(initBoard);
-  const [nextPlayerIsRed, setNextPlayerIsRed] = useState(false);
+  const [nextPlayerIs, setNextPlayerIs] = useState("Player1");
+  const [gameWinner, setGameWinner] = useState("");
 
+  // ゲームの状態の初期化
   const initGame = () => {
     let board = initBoard;
-    setBoard(board);
-    setNextPlayerIsRed(false);
+    setBoard(() => board);
+    setNextPlayerIs(() => "Player1");
+    setGameWinner(() => "");
   };
 
+  // ボードの深いコピーを作成
   const copyBoard = (board) => {
     let copiedBoard = [];
     for (const array of board) {
@@ -73,19 +79,54 @@ const GameDisplayPage = () => {
     return copiedBoard;
   };
 
-  // 置けるかどうかの判定を最初にする、別個に関数を用意して小分けにする
-  const handleClick = (event) => {
-    let nextBoard = copyBoard(board);
-    const dataset = event.currentTarget.dataset;
-    const x = dataset.x;
-    for (let y = 0; y < board.length; y++) {
-      if (nextBoard[x][y] == null) {
-        nextBoard[x][y] = nextPlayerIsRed;
-        setNextPlayerIsRed((prevState) => !prevState);
-        break;
+  // 選択した列に石が置けるか判定
+  const canPutStone = (board, x) => {
+    if (board[x][5] != null) return false;
+    return true;
+  };
+
+  // プレイヤーを変更
+  const changePlayer = (nextPlayerIs) => {
+    if (nextPlayerIs == "Player1") {
+      setNextPlayerIs(() => "Player2");
+    } else if (nextPlayerIs == "Player2") {
+      setNextPlayerIs(() => "Player1");
+    }
+  };
+
+  // 選んだ列の一番下の空いているy座標を返す
+  const getYIndex = (board, x) => {
+    for (let y = 0; y < 6; y++) {
+      if (board[x][y] == null) {
+        return y;
       }
     }
-    setBoard(nextBoard);
+  };
+
+  // 選んだ列の一番下に石を落とす。配列にはboolean値ではなくstringを入れる
+  const putStone = (board, x, y) => {
+    board[x][y] = nextPlayerIs;
+  };
+
+  const handleClick = (event) => {
+    if (gameWinner == "") {
+      let nextBoard = copyBoard(board);
+      let dataset = event.currentTarget.dataset;
+      let x = dataset.x;
+      if (canPutStone(nextBoard, x)) {
+        let y = getYIndex(nextBoard, x);
+        putStone(nextBoard, x, y);
+        // 勝利判定
+        let winner = calculateWinner(nextBoard, 4, x, y);
+        if (winner != null) {
+          setGameWinner(() => winner);
+          setBoard(() => nextBoard);
+        } else if (winner == null) {
+          changePlayer(nextPlayerIs);
+          setBoard(nextBoard);
+        }
+      }
+    }
   };
 
   return (
@@ -93,6 +134,7 @@ const GameDisplayPage = () => {
       <h1>Connect 4!</h1>
       <Button onClick={initGame} />
       <Board board={board} onClick={handleClick} />
+      <h1>{gameWinner}</h1>
       {/* それぞれの手番の情報を表示する */}
       <div className="game-info">
         <div>{/*status*/}</div>
