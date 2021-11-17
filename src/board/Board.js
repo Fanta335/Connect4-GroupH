44a23d81a56c726794cb8c09ba355bee9a85d7c5
YuyaNakamura0139 <mtk371@icloud.com>
2021-11-17 20:55:17 +0900

    #55 ゲーム画面をMaterial UIを用いて実装

--- src/board/Board.js
+++ src/board/Board.js
@@ -1,16 +1,15 @@
 import React from "react";
 import "./Board.css";
 import Column from "./../board/Column.js";
+import Grid from "@mui/material/Grid";
 
 const Board = (props) => {
   return (
-    <div>
-      <div className="flex-direction-row">
-        {props.board.map((x, i) => (
-          <Column column={x} key={i} x={i} onClick={props.onClick} />
-        ))}
-      </div>
-    </div>
+    <Grid sx={{ display: "flex", flexDirection: "row" }}>
+      {props.board.map((x, i) => (
+        <Column column={x} key={i} x={i} onClick={props.onClick} />
+      ))}
+    </Grid>
   );
 };
 
--- src/board/Cell.js
+++ src/board/Cell.js
@@ -1,17 +1,49 @@
 import React from "react";
 import "./Board.css";
+import Box from "@mui/material/Box";
+
+const styles = {
+  cell: {
+    height: "7rem",
+    width: "7rem",
+    background: "#1565c0",
+    cursor: "pointer",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  white: {
+    height: "5.5em",
+    width: "5.5em",
+    background: "white",
+    borderRadius: "100%",
+  },
+  red: {
+    height: "5.5em",
+    width: "5.5em",
+    background: "red",
+    borderRadius: "100%",
+  },
+  yellow: {
+    height: "5.5em",
+    width: "5.5em",
+    background: "yellow",
+    borderRadius: "100%",
+  },
+};
 
 const Cell = (props) => {
-  let color = "white";
+  let color = styles.white;
   if (props.value === "Player1") {
-    color = "red";
+    color = styles.red;
   } else if (props.value === "Player2") {
-    color = "yellow";
+    color = styles.yellow;
   }
   return (
-    <div className="cell" data-x={props.x} data-y={props.y} onClick={props.onClick}>
-      <div className={color}></div>
-    </div>
+    <Box sx={styles.cell} data-x={props.x} data-y={props.y} onClick={props.onClick}>
+      <Box sx={color}></Box>
+    </Box>
   );
 };
 
