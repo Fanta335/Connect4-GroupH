87066cc1049718ea4e2706ad445a6b8daa15df91
Yuki Ueno <y0uk10126@gmail.com>
2021-11-25 21:41:47 +0900

    #24 勝者をgameWinnerの名前で判定するように変更

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -17,6 +17,9 @@ const style = {
 };
 
 const BasicModal = (props) => {
+  const playerName1 = props.playerName1;
+  const playerName2 = props.playerName2;
+
   return (
     <div>
       <Modal
@@ -29,10 +32,10 @@ const BasicModal = (props) => {
           {props.gameWinner !== "draw" ? (
             <>
               <Typography id="modal-modal-title" variant="h6" component="h2">
-                The winner is {props.gameWinner}
+                The winner is {props.gameWinner === "Player1" ? playerName1 : playerName2}
               </Typography>
               <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                Congratulations! {props.gameWinner} won the game!
+                Congratulations! {props.gameWinner === "Player1" ? playerName1 : playerName2} win the game!
               </Typography>
             </>
           ) : (
