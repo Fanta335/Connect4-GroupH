a50e19f585b65698853565db62eadcd7d7196c10
Yuki Ueno <y0uk10126@gmail.com>
2021-11-25 21:42:07 +0900

    Boxをimportしてくるように修正

--- src/components/board/DisplayPlayerTurn.js
+++ src/components/board/DisplayPlayerTurn.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 const playerTurnStyle = {
