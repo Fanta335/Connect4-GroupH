46de3d2e334ba807c2a0126bd069feac32395a4d
Yuki Ueno <y0uk10126@gmail.com>
2021-11-27 01:34:01 +0900

    #24 描画負荷低減のため1/100秒単位から1秒単位に変更

--- src/utils/displayTimer.js
+++ src/utils/displayTimer.js
@@ -1,18 +1,15 @@
 /**
  * タイマーの時間表示
  * @param {number} count
- * @returns タイマー表示（mm:ss.00）
+ * @returns タイマー表示（mm:ss）
  */
 const displayTimer = (count) => {
   if (count >= 0) {
-    const minute = Math.floor(count / 100 / 60);
+    const minute = Math.floor(count / 60);
     const displayMinute = minute >= 10 ? String(minute) : "0" + String(minute);
-    const second = count % (60 * 100);
-    const secondFront = Math.floor(second / 100);
-    const secondBack = second % 100;
-    const displaySecondFront = secondFront >= 10 ? String(secondFront) : "0" + String(secondFront);
-    const displaySecondBack = secondBack >= 10 ? String(secondBack) : "0" + String(secondBack);
-    return `${displayMinute}:${displaySecondFront}:${displaySecondBack}`;
+    const second = count % 60;
+    const displaySecond = second >= 10 ? String(second) : "0" + String(second);
+    return `${displayMinute}:${displaySecond}`;
   } else {
     return "Time is up!";
   }
