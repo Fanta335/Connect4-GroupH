6e70506f742c2de0ae95d4b9ef22472e842afe23
Yuki Ueno <y0uk10126@gmail.com>
2021-11-28 21:41:21 +0900

    #84 関数の並びを変更

--- src/utils/calculateWinner.js
+++ src/utils/calculateWinner.js
@@ -1,30 +1,6 @@
 import canPutStone from "./canPutStone";
 
 /**
- * 勝者を返す関数
- * @param {string[][]} board - 盤面を表す二次元配列
- * @param {number} victoryCondition - 勝利条件
- * @param {number} x - 石を置いた座標x（横方向）
- * @param {number} y - 石を置いた座標y（縦方向）
- * @returns {string} 勝者
- */
-function calculateWinner(board, victoryCondition, x, y) {
-  // 勝者の判定
-  if (isConnectedN(board, victoryCondition, x, y)) {
-    return board[x][y];
-  }
-  // 引き分けの判定
-  let isGameContinue = false;
-  for (let x = 0; x < board.length; x++) {
-    isGameContinue = isGameContinue || canPutStone(board, x);
-    if (isGameContinue) {
-      return null;
-    }
-  }
-  return "draw";
-}
-
-/**
  * 座標(x, y)を中心にN個連続で並んでいるかどうか判定する関数
  * @param {string[][]} arr - 盤面を表す二次元配列
  * @param {number} victoryCondition - 勝利条件
@@ -33,9 +9,9 @@ function calculateWinner(board, victoryCondition, x, y) {
  * @returns {boolean} - true/false
  */
 function isConnectedN(arr, victoryCondition, x, y) {
-  let pattern = new RegExp(arr[x][y].repeat(victoryCondition));
-  let width = arr.length;
-  let height = arr[0].length;
+  const pattern = new RegExp(arr[x][y].repeat(victoryCondition));
+  const width = arr.length;
+  const height = arr[0].length;
 
   // 縦方向
   let str = "";
@@ -102,6 +78,30 @@ function isConnectedN(arr, victoryCondition, x, y) {
   }
 
   return false;
+}
+
+/**
+ * 勝者を返す関数
+ * @param {string[][]} board - 盤面を表す二次元配列
+ * @param {number} victoryCondition - 勝利条件
+ * @param {number} x - 石を置いた座標x（横方向）
+ * @param {number} y - 石を置いた座標y（縦方向）
+ * @returns {string} 勝者
+ */
+function calculateWinner(board, victoryCondition, x, y) {
+  // 勝者の判定
+  if (isConnectedN(board, victoryCondition, x, y)) {
+    return board[x][y];
+  }
+  // 引き分けの判定
+  let isGameContinue = false;
+  for (let i = 0; i < board.length; i++) {
+    isGameContinue = isGameContinue || canPutStone(board, i);
+    if (isGameContinue) {
+      return null;
+    }
+  }
+  return "draw";
 }
 
 export default calculateWinner;
