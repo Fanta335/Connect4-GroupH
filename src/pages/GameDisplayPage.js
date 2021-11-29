b80a23410c12c6a634385b919a832a657fdc07c7
奥田啓皓 <>
2021-11-29 22:34:22 +0900

    ゲームボードの大きさ、レスポンシブデザインを改善しました

--- src/pages/GameDisplayPage.js
+++ src/pages/GameDisplayPage.js
@@ -24,8 +24,14 @@ const useStyles = makeStyles({
     backgroundColor: "rgba(0,0,0,0.7)",
   },
   history: {
-    marginLeft: "40px",
     padding: theme.spacing(3),
+    marginTop: "40px",
+    [theme.breakpoints.up('md')]: {
+      marginLeft: "40px",
+    },
+    [theme.breakpoints.down('md')]: {
+      marginLeft: "0"
+    },
   },
   historyCard: {
     textAlign: "center",
@@ -386,10 +392,12 @@ const GameDisplayPage = (props) => {
           </Grid>
         </Card>
       </Grid>
+
       <Grid container justifyContent="center" style={{marginBottom: "200px"}}>
         <Grid
           item
           xs={10}
+          md={6}
         >
           <Board
             board={currentBoard}
