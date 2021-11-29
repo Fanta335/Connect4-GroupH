b4f25c4928e67111ca51afc095e6de65e0a15fbc
Yuki Ueno <y0uk10126@gmail.com>
2021-11-29 22:49:35 +0900

    #86 ゲーム初回開始時のモーダル追加とデザインの調整

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -13,54 +13,81 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "background.paper",
-  border: "2px solid #000",
   boxShadow: 24,
+  opacity: 0.87,
   p: 4,
 };
 
-const BasicModal = (props) => {
+const GameStartModal = (props) => (
+  <Modal
+    open={props.open}
+    onClose={props.handleClose}
+    aria-labelledby="modal-modal-title"
+    aria-describedby="modal-modal-description"
+  >
+    <Box item sx={style}>
+      <Grid container spacing={2}>
+        <Grid item sm={12} alignItems="center">
+          <Typography id="modal-modal-title" variant="h4" align="center" color="primary">
+            Are you ready?
+          </Typography>
+        </Grid>
+        <Grid item sm={12}>
+          <TransitionButton
+            name="Start"
+            onClick={() => {
+              props.handleStart();
+              props.handleClose();
+            }}
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  </Modal>
+);
+
+const GameFinishModal = (props) => {
   const [playerName1, playerName2] = props.players;
+  const gameWinner = props.gameWinner;
+  let statement;
+  if (gameWinner === "Player1") {
+    statement = `${playerName1} Wins!!`;
+  } else if (gameWinner === "Player2") {
+    statement = `${playerName2} Wins!!`;
+  } else if (gameWinner === "draw") {
+    statement = "It's a tie!!";
+  }
   const navigate = useNavigate();
   return (
-    <div>
-      <Modal
-        open={props.open}
-        onClose={props.handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          {props.gameWinner !== "draw" ? (
-            <>
-              <Typography id="modal-modal-title" variant="h6" component="h2">
-                The winner is {props.gameWinner === "Player1" ? playerName1 : playerName2}
-              </Typography>
-              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                Congratulations! {props.gameWinner === "Player1" ? playerName1 : playerName2} win the game!
-              </Typography>
-            </>
-          ) : (
-            <>
-              <Typography id="modal-modal-title" variant="h6" component="h2">
-                Draw
-              </Typography>
-              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                This game is a draw. Try again!
-              </Typography>
-            </>
-          )}
-          <Grid container item alignItems="center" justifyContent="center" spacing={3}>
-            <Grid item sm={6}>
-              <TransitionButton name="Back to Home" onClick={() => navigate("/")} />
-            </Grid>
-            <Grid item sm={6}>
-              <TransitionButton name="Retry" onClick={props.handleClose} />
-            </Grid>
+    <Modal
+      open={props.open}
+      onClose={props.handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box item sx={style}>
+        <Grid container spacing={2}>
+          <Grid item sm={12} alignItems="center">
+            <Typography id="modal-modal-title" variant="h4" align="center" color="primary">
+              {statement}
+            </Typography>
           </Grid>
-        </Box>
-      </Modal>
-    </div>
+          <Grid item sm={6}>
+            <TransitionButton name="Back to Home" onClick={() => navigate("/")} />
+          </Grid>
+          <Grid item sm={6}>
+            <TransitionButton
+              name="Retry"
+              onClick={() => {
+                props.handleStart();
+                props.handleClose();
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+    </Modal>
   );
 };
 
-export default BasicModal;
+export { GameStartModal, GameFinishModal };
