19551a8d91270fcda8094794a09ebe16ad80648d
Yuki Ueno <y0uk10126@gmail.com>
2021-11-30 00:52:37 +0900

    #70 propsの追加

--- src/components/board/Column.js
+++ src/components/board/Column.js
@@ -7,15 +7,19 @@ import Cell from "./Cell";
 import "./Board.css";
 
 const Column = (props) => (
-    <Grid
-      item
-      sx={{ display: "flex", flexDirection: "column-reverse" }}
-      xs={12}
-    >
-      {props.column.map((y, i) => (
-        <Cell value={y} key={i} x={props.x} y={i} onClick={props.onClick} />
-      ))}
-    </Grid>
+  <Grid item sx={{ display: "flex", flexDirection: "column-reverse" }} xs={12}>
+    {props.column.map((y, i) => (
+      <Cell
+        value={y}
+        key={i}
+        x={props.x}
+        y={i}
+        playerColor1={props.playerColor1}
+        playerColor2={props.playerColor2}
+        onClick={props.onClick}
+      />
+    ))}
+  </Grid>
 );
 
 export default Column;
