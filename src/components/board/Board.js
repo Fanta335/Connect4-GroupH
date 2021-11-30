ed22cfd9e0b63b63f7249902ebf15daa9b9c278b
Yuki Ueno <y0uk10126@gmail.com>
2021-12-01 00:00:31 +0900

    #132 ダークモードの切り替えをスイッチからライトマークに変更しました

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -6,9 +6,9 @@ import { makeStyles } from "@mui/styles";
 import Column from "./Column";
 
 const useStyles = makeStyles((theme) => ({
-  board : {
-    boxShadow: "inset 0 10px 0 rgba(255,255,255,0.2), 0 10px 10px rgba(0, 0, 0, 0.19)"
-  }
+  board: {
+    boxShadow: "inset 0 10px 0 rgba(255,255,255,0.2), 0 10px 10px rgba(0, 0, 0, 0.19)",
+  },
 }));
 
 const Board = (props) => {
@@ -16,17 +16,18 @@ const Board = (props) => {
 
   return (
     <Grid item sx={{ display: "flex", flexDirection: "row" }} xs={12} className={classes.board}>
-    {props.board.map((x, i) => (
-      <Column
-        column={x}
-        key={i}
-        x={i}
-        playerColor1={props.playerColor1}
-        playerColor2={props.playerColor2}
-        onClick={props.onClick}
-      />
-    ))}
-  </Grid>);
+      {props.board.map((x, i) => (
+        <Column
+          column={x}
+          key={i}
+          x={i}
+          playerColor1={props.playerColor1}
+          playerColor2={props.playerColor2}
+          onClick={props.onClick}
+        />
+      ))}
+    </Grid>
+  );
 };
 
 export default Board;
